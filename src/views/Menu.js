b21a5bddd88e59f50c17374633af259bad5b42a4
IvaTsu <ivan.tsud@gmail.com>
2018-03-07 10:24:16 +0200

    init project

---
+++ src/views/Menu.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './style.css';
+
+const Menu = () => (
+  <nav>
+    <div className={styles['toggle-btn']}>Hello</div>
+  </nav>
+);
+
+export default Menu;
